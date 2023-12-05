d5f223034eb8abf0d53998cbb48eda2fb0c0afc5
Lizperdomo <129300353+Lizperdomo@users.noreply.github.com>
2023-12-04 23:14:16 -0600

    seguros.sql

--- seguros.sql
+++ seguros.sql
@@ -1,135 +1,136 @@
-create database seguros;
- 
-use seguros;
+create database primaria;
 
-SELECT @@datadir;
+drop database primaria;
 
-drop database seguros;
+use primaria;
 
-create table coberturas(
+create table admin(
 	id int not null auto_increment,
-	nombre varchar(50) unique,
-	descripcion varchar(100),
-	status varchar(20) not null,
-	costo float not null,
-	created_at date,
-	updated_at datetime null,
-	deleted_at datetime null,
-	primary key(id)
+	primary key(id),
+	usuario varchar(20) not null,
+	contraseña varchar(20) not null,
+	created_at datetime not null,
+    updated_at datetime null,
+    deleted_at datetime null
 );
 
-create table usuarios(
-	id int not null auto_increment,
-	nombre varchar(30) not null,
-	apellidoP varchar(30) not null,
-	apellidoM varchar(30) not null,
-	fechaNac date not null,
-	calle varchar(50) null,
-	colonia varchar(50) null,
-	municipio varchar(50) null,
-	estado varchar(50) null, 
-	ciudad varchar(50) null,
-	CURP varchar(30) not null unique,
-	RFC varchar(20) not null unique,
-	telefono varchar(10) not null,
-	perfil varchar(20) not null,
-	nombreUsuario varchar(50) not null,
-	contraseña varchar(50) not null,
-	idCobertura int null,
-	index(idCobertura),
-	created_at datetime,
-	updated_at datetime null,
-	deleted_at datetime null,
-	foreign key(idCobertura) references coberturas(id),
-	primary key(id)
+create table maestros(
+    id int not null auto_increment,
+    primary key(id),
+    usuario varchar(50) not null,
+    contraseña varchar(20) not null,
+    nombre varchar(50) not null,
+    aPaterno varchar(50) not null,
+    aMaterno varchar(50) not null,
+    calle varchar(30) not null,
+    colonia varchar(30) not null,
+    numero int not null,
+    municipio varchar(50) not null,
+    ciudad varchar(50) not null,
+    estado varchar(50) not null,
+    matricula varchar(30) not null unique,
+    telefono varchar(10) not null,
+    created_at datetime not null,
+    updated_at datetime null,
+    deleted_at datetime null
+);
+
+create table alumnos(
+    id int not null auto_increment,
+    primary key(id),
+    usuario varchar(50) not null,
+    contraseña varchar(20) not null,
+    nombre varchar(50) not null, 
+    aPaterno varchar(50) not null,
+    aMaterno varchar(50) not null,
+    numeroControl int not null unique,
+    fechaN date not null,
+    calle varchar(50) not null,
+    colonia varchar(50) not null,
+    numero int null,
+    municipio varchar(50) not null,
+    ciudad varchar(30) not null,
+    estado varchar(30) not null,
+    grado varchar(20) not null,
+    grupo varchar(20) not null,
+    created_at datetime not null,
+    updated_at datetime null,
+    deleted_at datetime null
+);
+
+create table materias(
+    id int not null auto_increment,
+    nombre varchar(50) not null,
+    nombreCorto varchar(10) not null,
+    clave varchar(10) not null,
+    unidades int not null,
+    maestro int not null,
+    index(maestro),
+    created_at datetime not null,
+    updated_at datetime not null,
+    deleted_at datetime null,
+    foreign key(maestro) references maestros(id),
+    primary key(id)
 );
 
 
-create table usuario_cobertura(
-	id int not null auto_increment primary key,
-	usuario int not null,
-	index(usuario),
-	cobertura int not null,
-	index(cobertura),
-	foreign key(usuario) references usuarios(id),
-	foreign key(cobertura) references coberturas(id)
+create table calificaciones(
+    id int not null auto_increment,
+    alumno int not null,
+    index(alumno),
+    español float null,
+    matematicas float null,
+    historia float null,
+    civismo float null,
+    edFisica float null,
+    geografia float null,
+    ciencias float null,
+    created_at datetime not null,
+    updated_at datetime null,
+    deleted_at datetime null,
+    foreign key(alumno) references alumnos(id),
+    primary key(id)
 );
+   
+
+insert INTO admin (usuario, contraseña, created_at) -- Ya 
+VALUES 
+    ('isa', '150503', '2023/11/05 11:52:02');
+   
+insert INTO maestros (usuario, contraseña, nombre, aPaterno, aMaterno, calle, colonia, numero, municipio, ciudad, estado, matricula, telefono, created_at) -- Ya
+VALUES 
+    ('xime', '1234xime', 'Ximena', 'Arcos', 'Perdomo', 'San andrés', 'Fovissste', 2, 'Teziutlán', 'Puebla', 'Puebla', 'XI125684', 2311256984, '0000/00/00 00:00:00'),
+    ('val', 'vale003', 'Valeria', 'Guerrero', 'Juárez', 'Hidalgo', 'Centro', 15, 'Jalacingo', 'Veracruz', 'Veracruz', 'VA297162', 2321568475, '0000/00/00 00:00:00'),
+    ('luis', 'lu03985', 'Luis', 'Linos', 'Huerta', '16 de septiembre', 'Juárez', 56, 'Teziutlán', 'Puebla', 'Puebla', 'LU365130', 2311684571, '0000/00/00 00:00:00'),
+    ('marce', 'mar3698', 'Marcelo', 'Arcos', 'Vidal', 'Bugambilias', 'Norte', 232, 'Zacapoaxtla', 'Puebla', 'Puebla', 'MA314100', 2351694606, '0000/00/00 00:00:00'),
+    ('gaby', 'gab2695', 'Gabriela', 'Torres', 'Dinorin', 'Aquiles serdán', 'Sur', 1056, 'Jalacingo', 'Veracruz', 'Veracruz', 'GA329024', 2321540695, '0000/00/00 00:00:00');
+
+   
+INSERT INTO alumnos (usuario, contraseña, nombre, aPaterno, aMaterno, numeroControl, fechaN, calle, colonia, numero, municipio, ciudad, estado, grado, grupo, created_at) -- Ya
+VALUES 
+    ('alex', 'alex2023', 'Alex', 'Huerta', 'Sánchez', 150, '2011/11/15', 'Julio', 'Bugambilias', 26, 'Zaragoza', 'Puebla', 'Puebla', 'Sexto', 'A', '0000/00/00 00:00:00'),
+    ('ale', 'ale2023', 'Alejandra', 'Duran', 'Pérez', 265, '2012/12/08', 'Hidalgo', 'Libertad', 220, 'Teziutlán', 'Puebla', 'Puebla', 'Cuarto', 'B', '0000/00/00 00:00:00'),
+    ('keni', 'keni2023', 'Kenia', 'Arroyo', 'Espinoza', 365, '2013/06/14', 'Insurgentes', 'Centro', 180, 'Jalacingo', 'Veracruz', 'Veracruz', 'Quinto', 'C', '0000/00/00 00:00:00'),
+    ('chevis', 'chevis2023', 'Sebastián', 'Flores', 'Flores', 165, '2017/08/25', 'Industrial', 'Juárez', 250, 'Monterrey', 'Nuevo León', 'Nuevo León', 'Primero', 'B', '0000/00/00 00:00:00'),
+    ('caro', 'caro2023', 'Carolina', 'Gómez', 'Pantoja', 564, '2015/04/25', 'Castillo', 'León', 1254, 'Zacapoaxtla', 'Puebla', 'Puebla', 'Tercero', 'A', '0000/00/00 00:00:00');
 
 
-create table ventas(
-	id int not null auto_increment,
-	numeroTarjeta int not null unique,
-	cvv int not null unique, 
-	monto float not null,
-	usuario int not null,
-	index(usuario),
-	cobertura int not null,
-	index(cobertura),
-	created_at datetime,
-	updated_at datetime null,
-	deleted_at datetime null,
-	foreign key(usuario) references usuarios(id),
-	foreign key(cobertura) references coberturas(id),
-	primary key(id)
-);
+INSERT INTO materias (nombre, nombreCorto, clave, unidades, maestro, created_at) -- foreign key(maestro) references maestros(id)
+VALUES 
+    ('Español', 'ESP', 'ESP-1', 5, 1, '0000/00/00 00:00:00 '),
+    ('Matemáticas', 'MAT', 'MAT-1', 4, 2, '0000/00/00 00:00:00'),
+    ('Historia', 'HIST', 'HIST-2', 3, 3, '0000/00/00 00:00:00'),
+    ('Civismo', 'CIVIS', 'CIVIS-4', 5, 4, '0000/00/00 00:00:00'),
+    ('Educación Física', 'EDUFI', 'EDUFI-3', 6, 5, '0000/00/00 00:00:00'),
+    ('Geografía', 'GEO', 'GEO-3', 4, 2, '0000/00/00 00:00:00'),
+    ('Ciencias Naturales', 'CIEN', 'CIEN-1', 5, 3, '0000/00/00 00:00:00');
 
-create table cobertura_venta(
-	id int not null auto_increment primary key,
-	cobertura int not null,
-	index(cobertura),
-	venta int not null,
-	index(venta),
-	foreign key(cobertura) references coberturas(id),
-	foreign key(venta) references ventas(id)
-);
-
-insert into coberturas (nombre, descripcion, status, costo, created_at)
-	values 	('Enfermedades graves', 'Esta cobertura cubre hasta 8 enfermedades', 'Disponible', 150, '2023/07/16'),
-			('Cirugias', 'Esta cobertura cubre hasta 20 cirugias', 'No disponible', 250, '2023/07/19'),
-			('Gastos funerarios', 'Esta cobertura te ofrece hasta $54000 pesos para gastos funerarios', 'Disponible', 200, '2023/07/19'),
-			('Accidentes personales', 'Esta cobertura ofrece hasta $85000 pesos', 'Disponible', 250, '2023/07/20'),
-			('Garantía escolar', 'Esta cobertura cubre varios gastos escolares', 'Disponible', 100, '2023/07/22'),
-			('Protección contra cáncer', 'Esta cobertura cubre hasta 10 quimioterapias', 'Disponible', 150, '2023/07/22'),
-			('Enfermedades terminales', 'Esta cobertura ofrece hasta $1000000 de pesos', 'No disponible', 300, '2023/07/24'),
-			('Apoyo por hospitalización', 'Esta cobertura ofrece pagar todos los gatos de hospitalización', 'Disponible', 250, '2023/07/25'),
-			('Muerte accidental', 'Esta cobertura ofrece a los familiares del asegurado hasta $100000 pesos ', 'No disponible', 150, '2023/07/26'),
-			('Fallecimiento', 'En caso de fallecer el asegurado se les entrega a los familiares el dinero ahorrado', 'Disponible', 350, '2023/07/28');
-	
-		select * from coberturas;
-	
-insert into usuarios (nombre, apellidoP, apellidoM, fechaNac, calle, colonia, municipio, estado, ciudad, CURP, RFC, telefono, perfil, nombreUsuario, contraseña, idCobertura, created_at)
-	values 	('Lizbeth', 'Perdomo', 'González', '1998/08/09', '5 de mayo', 'Centro', 'Teziutlán', 'Puebla', 'Puebla', 'PEGL080903MPLRNZA9', 'PEGL080903TE2', '2311202681', 'Administrador', 'Liz', 'liz080903', null , '2023/07/20'), 
-			('Monserrat', 'Peña', 'Guerrero', '1973/10/25', 'Allende', 'Centro', 'Teziutlán ', 'Puebla', 'Puebla', 'MOPG980404MPLHY6C', 'MONPG980404MPLU', '2311256584', 'Cliente', 'Peña', 'MOPG980404', 1, '2023/07/20'),
-			('Luis', 'Sánchez', 'Arriaga', '1990/05/15', 'Hidalgo', 'Centro', 'Teziutlán', 'Puebla', 'Puebla', 'LUSA200714HPLDF5K', 'LUSA200714HPLDY', '2316953210', 'Cliente', 'Sanchez', 'LUSA200714',  3, '2023/07/22'),
-			('Lucia', 'Jiménez', 'Zamora', '1984/06/12','Nacozari', 'El carmen', 'Jalacingo', 'Veracruz', 'Veracruz', 'LUJZ971004MPLOK5H', 'LUJZ971004MPLFI', '2321584102', 'Cliente', 'Jimenez', 'LUJZ971004', 5, '2023/07/24'),
-			('Mariana', 'Aguilar', 'Velazquez', '1990/11/26','Julio', 'Fresnillo', 'San juan', 'Puebla', 'Puebla', 'MAAV060608MPLDJNS', 'MAAV060608MPLDB', '2311574200', 'Cliente', 'Aguilar', 'MAAV060608', 10, '2023/07/27'),
-			('Israel', 'Periañez', 'Rosas', '1995/07/06','Los pinos', 'Ciprecess', 'Jalacingo', 'Veracruz', 'Veracruz', 'ISPR030515HPLUYBJ', 'ISPR030515HPLHU', '2320685231', 'Cliente', 'Periañez', 'ISPR030515', 8, '2023/07/28');
-		
-		select * from usuarios;
-
-insert into usuario_cobertura (usuario, cobertura)
-	values 	(2, 1),
-			(3, 3),	
-			(4, 5),
-			(5, 6),
-			(6, 8);
-	
-insert into ventas (numeroTarjeta, cvv, monto, usuario, cobertura, created_at)
-	values 	(16598235, 126, 150, 2, 1, '2023/07/20'),
-			(16595952, 456, 200, 3, 3, '2023/07/22'),
-			(69512263, 149, 100, 4, 5, '2023/07/24'),
-			(26975236, 364, 350, 5, 10, '2023/07/27'),
-			(14861415, 265, 250, 2, 2, '2023/07/27'),
-			(26461115, 266, 150, 3, 6, '2023/07/27'),
-			(36564866, 025, 250, 5, 4, '2023/07/27'),
-			(20684994, 648, 300, 4, 7, '2023/07/27'),
-			(78626100, 984, 150, 4, 9, '2023/07/27'),
-			(98262163, 658, 250, 5, 8, '2023/07/28');
-
-	
-insert into cobertura_venta (cobertura, venta)
-	values 	(1, 1),
-			(3, 2),
-			(5, 3),
-			(6, 4),
-			(8, 5);
+INSERT INTO calificaciones (alumno, español, matematicas, historia, civismo, edFisica, geografia, ciencias, created_at) --  foreign key(alumno) references alumnos(id),
+VALUES 
+    (1, 9.5, 8, 7.9, 9, 10, 9.0, 9.08, '2023/10/11'),
+    (2, 8.5, 9, 7.0, 8, 9, 8.5, 8.25, '2023/10/12'),
+	(3, 9.2, 8.8, 9.0, 8.5, 9.3, 8.9, 8.93, '2023/10/13'),
+    (4, 8.9, 9.1, 8.7, 9.2, 9.0, 8.8, 8.93, '2023/10/14');
+   
+   show databases;
+  
